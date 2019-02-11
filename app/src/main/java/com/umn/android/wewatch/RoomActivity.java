0818733dc416e0feda8dccb90cdb14ef73ed39ec
ryubears <ryubears@gmail.com>
2019-02-11 00:08:20 -0600

    Setup Youtube Player

--- app/src/main/java/com/umn/android/wewatch/RoomActivity.java
+++ app/src/main/java/com/umn/android/wewatch/RoomActivity.java
@@ -1,13 +1,34 @@
 package com.umn.android.wewatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class RoomActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class RoomActivity extends YouTubeBaseActivity {
+    private YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+
+        youTubePlayerView = findViewById(R.id.room_youtube_player);
+
+        youTubePlayerView.initialize(BuildConfig.YOUTUBE_API_KEY,
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                        youTubePlayer.cueVideo("gnLZFfgNEX8");
+                    }
+
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                        Log.e("RoomActivity", "Youtube initialization failed.");
+                    }
+                });
     }
 }
