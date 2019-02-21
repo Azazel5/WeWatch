2aa5906b4b35351098550f3bd595a67ef94c2020
ryubears <ryubears@gmail.com>
2019-02-21 14:37:54 -0600

    Change YouTube Player Library

--- app/src/main/java/com/umn/android/wewatch/MainActivity.java
+++ app/src/main/java/com/umn/android/wewatch/MainActivity.java
@@ -1,8 +1,8 @@
 package com.umn.android.wewatch;
 
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProviders;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import javax.inject.Inject;
--- app/src/main/java/com/umn/android/wewatch/RoomActivity.java
+++ app/src/main/java/com/umn/android/wewatch/RoomActivity.java
@@ -1,14 +1,15 @@
 package com.umn.android.wewatch;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class RoomActivity extends YouTubeBaseActivity {
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class RoomActivity extends AppCompatActivity {
     private YouTubePlayerView youTubePlayerView;
 
     @Override
@@ -17,18 +18,14 @@ public class RoomActivity extends YouTubeBaseActivity {
         setContentView(R.layout.activity_room);
 
         youTubePlayerView = findViewById(R.id.room_youtube_player);
+        getLifecycle().addObserver(youTubePlayerView);
 
-        youTubePlayerView.initialize(BuildConfig.YOUTUBE_API_KEY,
-                new YouTubePlayer.OnInitializedListener() {
-                    @Override
-                    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                        youTubePlayer.cueVideo("gnLZFfgNEX8");
-                    }
-
-                    @Override
-                    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                        Log.e("RoomActivity", "Youtube initialization failed.");
-                    }
-                });
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "S0Q4gqBUs7c";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
     }
 }
